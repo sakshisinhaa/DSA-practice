6caf102565861d865448a71bb8fde9dea36e4c17
sakshisinhaa <sakshiruchikaa@gmail.com>
2025-11-03 12:16:36 +0530

    traversal operation in array

---
+++ array-traversal.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={1,2,3,4,5};
+    int n= 5;
+    cout<<"array elements: ";
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
