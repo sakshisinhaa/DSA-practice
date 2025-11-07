bdc7234a678ddce69c7ddebe39495c2513a0d2a6
sakshisinhaa <sakshiruchikaa@gmail.com>
2025-11-07 13:13:16 +0530

    ques 215

---
+++ leetcode questions/ques 215.cpp
@@ -0,0 +1,8 @@
+//currently using sorting will solve the same question with heap in future
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end(),greater<int>());
+        return nums[k-1];
+    }
+};
