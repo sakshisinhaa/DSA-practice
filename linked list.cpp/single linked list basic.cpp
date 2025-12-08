a1c15c2672feaf3bbd6154f992fd19ea47ad0f62
sakshisinhaa <sakshiruchikaa@gmail.com>
2025-12-08 23:21:25 +0530

    made a linked list using functions

---
+++ linked list.cpp/single linked list basic.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node* next;
+};
+Node* head=NULL;
+void insert(int n){
+    Node* newNode = new Node();
+    newNode -> data = n;
+    newNode -> next = head;
+    head=newNode;
+}
+void print(){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<< temp->data<<" ";
+        temp = temp->next;
+    }
+}
+int main(){
+    insert(2);
+    insert(3);
+    insert(6);
+    insert(5);
+    insert(9);
+    print();
+}
