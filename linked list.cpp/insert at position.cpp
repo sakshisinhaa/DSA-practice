3e25ce93ecc003b9f09e0139e8b3074e43b54be0
sakshisinhaa <sakshiruchikaa@gmail.com>
2025-12-09 09:51:35 +0530

    made some changes in insert at position code

--- linked list.cpp/insert at position.cpp
+++ linked list.cpp/insert at position.cpp
@@ -47,3 +47,48 @@ int main(){
     cout<<"Linked List after insertion: ";
     traversal(head);
 }
+
+#include<iostream>
+// using namespace std;
+// struct Node{
+//     int data;
+//     Node* next;
+// };
+// Node* head= NULL;
+// void insert(int n){
+//     Node* newNode = new Node();
+//     newNode->data = n;
+//     newNode->next = head;
+//     head=newNode;
+// }
+// void insertAtPosition(int pos, int n){
+//     Node* newNode = new Node();
+//     newNode->data=n;
+//     if(pos==1){
+//         newNode->next = head;
+//         head=newNode;
+//     }
+//     Node* temp = head;
+//     for(int i=1; i<pos-1 && temp != NULL; i++){
+//         temp =temp->next;
+//     }
+//     if(temp == NULL){
+//         cout<<"Invalid Position!"<<endl;
+//     }
+//     newNode->next = temp->next;
+//     temp->next=newNode;
+// }
+// void print(){
+//     Node* temp=head;
+//     while(temp != NULL){
+//         cout<<temp->data<<" ";
+//         temp = temp->next;
+//     }
+// }
+// int main(){
+//     insert(4);
+//     insert(3);
+//     insert(1);
+//     insertAtPosition(2,2);
+//     print();
+// }
