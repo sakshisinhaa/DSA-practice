0aea9c43738cc99b26dba0d9fe3d44d08268ddf2
sakshisinhaa <sakshiruchikaa@gmail.com>
2025-12-16 16:05:01 +0530

    double linked list

---
+++ linked list.cpp/double linked list.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node* next;
+    Node* prev;
+};
+Node* head = NULL;
+void insert(int n){
+    Node* newNode = new Node();
+    newNode->data = n;
+    newNode->next = head;
+    newNode->prev = NULL;
+    if(head!=NULL){
+        head->prev = newNode;
+    }
+    head = newNode;
+}
+void print(){
+    Node* temp= head;
+    while(temp != NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+}
+int main(){
+    insert(5);
+    insert(4);
+    insert(3);
+    insert(2);
+    insert(1);
+    print();
+}
