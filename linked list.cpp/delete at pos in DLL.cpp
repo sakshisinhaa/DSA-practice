792176cf97e0dda23adde8217fdb6fc678e345bb
sakshisinhaa <sakshiruchikaa@gmail.com>
2025-12-20 11:02:13 +0530

    delete node at position in a double linked list

---
+++ linked list.cpp/delete at pos in DLL.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node* next;
+    Node* prev;
+};
+Node* head= NULL;
+void insert(int n){
+    Node* newNode = new Node();
+    newNode->data = n;
+    newNode->next=head;
+    newNode-> prev = NULL;
+    if(head!=NULL){
+        head->prev = newNode;
+    }
+    head=newNode;
+}
+void deleteAtEnd(){
+    if(head==NULL){
+        cout<<"List is empty!"<<endl;
+        return;
+    }
+    Node* temp =head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    if(temp->prev != NULL){
+        temp->prev->next=NULL;
+    }else{
+        head=NULL;
+    }
+    delete temp;
+}
+void deleteAtPos(int pos){
+    if(head==NULL){
+        cout<<"List is empty, nothing to delete!"<<endl;
+        return;
+    }
+    Node* temp = head;
+    if(pos==1){
+        head=head->next;
+        if(head!=NULL){
+            head->prev=NULL;
+        }
+        delete head;
+        return;
+    }
+    for(int i=1; i<pos && temp!=NULL; i++){
+        temp=temp->next;
+    }
+    if(temp==NULL){
+        cout<<"Position out of range!";
+        return;
+    }
+    if(temp->next!=NULL){
+        temp->next->prev=temp->prev;
+    }
+    if(temp->prev!=NULL){
+        temp->prev->next=temp->next;
+    }
+    delete temp;
+}
+void print(){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<<temp -> data<<" ";
+        temp = temp->next;
+    }
+}
+int main(){
+    insert(5);
+    insert(4);
+    insert(3);
+    insert(2);
+    insert(1);
+    deleteAtPos(3);
+    print();
+}
